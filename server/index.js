d7995ceef2a46fba7d93d1b75b726d60bc70014c
yangxiaoping <2878847069@qq.com>
2024-05-08 21:07:45 +0800

    feat: 修改为setTimeout

--- server/index.js
+++ server/index.js
@@ -12,16 +12,32 @@ app.get('/api/sse', (req, res) => {
 
   const startTime = Date.now()
 
-  const timer = setInterval(() => {
+  // const timer = setInterval(() => {
+  //   if (Date.now() - startTime >= 10000) {
+  //     res.write('event:close\ndata:{}\n\n')
+  //     res.end()
+  //     clearInterval(timer)
+  //     return
+  //   }
+  //   const data = { message: 'HelloWorld', timestamp: Date.now() }
+  //   res.write(`data:${JSON.stringify(data)}\n\n`)
+  // }, 1000)
+
+  // https://juejin.cn/post/7355666189475954725
+  const sendEvent = () => {
+    // 检查是否已经发送了10秒
     if (Date.now() - startTime >= 10000) {
-      res.write('event:close\ndata:{}\n\n')
-      res.end()
-      clearInterval(timer)
+      res.write('event: close\ndata: {}\n\n') // 发送一个特殊事件通知客户端关闭
+      res.end() // 关闭连接
       return
     }
-    const data = { message: 'HelloWorld', timestamp: Date.now() }
-    res.write(`data:${JSON.stringify(data)}\n\n`)
-  }, 1000)
+    const data = { message: 'Hello World', timestamp: new Date() }
+    res.write(`data: ${JSON.stringify(data)}\n\n`)
+    // 每隔2秒发送一次消息
+    setTimeout(sendEvent, 2000)
+  }
+
+  sendEvent()
 })
 
 app.listen(PORT, () => {
