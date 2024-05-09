1c0fd531a262848572cd1fd6d555719c8160a8de
yangxiaoping <2878847069@qq.com>
2024-05-09 20:56:37 +0800

    feat: 补充axios

--- src/views/SSE.vue
+++ src/views/SSE.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   data () {
@@ -20,29 +20,40 @@ export default {
   },
   created () {
     const URL = 'http://localhost:3000/api/sse'
-    const source = new EventSource(URL)
-
-    source.onmessage = event => {
-      const { data } = event
-      const parseData = JSON.parse(data)
-      const { message, timestamp } = parseData
-      this.sseList.push(`Message: ${message} at ${timestamp}`)
-      if (message === 'close') {
-        // 一定要主动close 否则浏览器会间隔性的无休止调用SSE
-        source.close()
-      }
-    }
-
-    source.onerror = event => {
-      console.error(event)
-    }
-
-    source.addEventListener('foo', event => {
-      // 后续的不间断触发 会在请求头中设置字段Last-Event-Id
-      console.warn(event.data, event.lastEventId)
-    })
+    this.createAxios(URL)
+    // this.createSSE(URL)
   },
   methods: {
+    // axios会在res.send()触发后 接收到全量message 而非逐个接收
+    // [axios目前不支持接收stream](https://github.com/axios/axios/issues/479)
+    createAxios (url) {
+      return axios.get(url).then(res => {
+        console.log(res)
+      })
+    },
+    createSSE (url) {
+      const source = new EventSource(url)
+
+      source.addEventListener('message', event => {
+        const { data } = event
+        const parseData = JSON.parse(data)
+        const { message, timestamp } = parseData
+        this.sseList.push(`Message: ${message} at ${timestamp}`)
+        if (message === 'close') {
+          // 一定要主动close 否则浏览器会间隔性的无休止调用SSE
+          source.close()
+        }
+      })
+
+      source.addEventListener('error', event => {
+        console.error(event)
+      })
+
+      source.addEventListener('foo', event => {
+        // 后续的不间断触发 会在请求头中设置字段Last-Event-Id
+        console.warn(event.data, event.lastEventId)
+      })
+    }
   }
 }
 </script>
