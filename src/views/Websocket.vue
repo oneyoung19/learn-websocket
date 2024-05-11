efb0d7c7cbe2674300e2ca95364ce6d4a494cb6b
yangxiaoping <2878847069@qq.com>
2024-05-11 15:58:39 +0800

    feat: 添加浏览器websocket的API

--- src/views/Websocket.vue
+++ src/views/Websocket.vue
@@ -12,11 +12,29 @@ export default {
   },
   mounted () {
     const URL = 'http://localhost:3000/api/websocket'
+    const WS_URL = 'ws://localhost:3000/api/websocket'
     axios.get(URL).then(res => {
       console.log(res)
     })
+    this.createWebsocket(WS_URL)
   },
   methods: {
+    createWebsocket (url) {
+      const ws = new WebSocket(url)
+      console.log(ws)
+      ws.addEventListener('open', (event) => {
+        console.log('open', event)
+      })
+      ws.addEventListener('message', (event) => {
+        console.log('message', event)
+      })
+      ws.addEventListener('close', (event) => {
+        console.log('close', event)
+      })
+      ws.addEventListener('error', (event) => {
+        console.log('error', event)
+      })
+    }
   }
 }
 </script>
